60035aad136c1d52051693279c954f218d9256d8
huang yi <huangered@hotmail.com>
2017-05-31 08:30:41 +0800

    Update record

--- apps/game/include/record.hrl
+++ apps/game/include/record.hrl
@@ -1,7 +1,8 @@
--record(sock, {socket, transport}).
+-record(sock, {socket, transfer}).
 
 %% the record to store user, account, player, world info
 -record(user_detail, {user_id, player_id, world_id, player_proc, world_proc}).
+-record(user_socket, {user_id, socket, transfer}).
 
 -record(user, {id, username, password}).
 
